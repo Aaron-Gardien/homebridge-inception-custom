ac61f44995a9a9f6ad89ab49c0a800d7667f69a4
Aaron-Gardien <aaron.gardien@gmail.com>
2025-02-23 21:35:03 +1000

    Updated long polling to use correct request body for monitoring updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Version 2.0 - Improved handling of empty poll responses and HTTP errors
+// Version 2.3 - Updated long polling to use correct request body for monitoring updates
 const request = require('request');
 
 let Service, Characteristic;
@@ -21,9 +21,9 @@ class InceptionAccessory {
         
         this.service = new Service.SecuritySystem(config.name);
         
-        this.getAlarmState = (callback) => this._getAlarmState(callback);
-        this.setAlarmState = (value, callback) => this._setAlarmState(value, callback);
-        this.startLongPolling = () => this._startLongPolling();
+        this.getAlarmState = this.getAlarmState.bind(this);
+        this.setAlarmState = this.setAlarmState.bind(this);
+        this.startLongPolling = this.startLongPolling.bind(this);
         this.lookupAreaId();
 
         this.service
@@ -71,7 +71,7 @@ class InceptionAccessory {
         });
     }
 
-    _startLongPolling() {
+    startLongPolling() {
         this.log('[INFO] Starting long polling for state updates...');
         this.pollState();
     }
@@ -83,17 +83,46 @@ class InceptionAccessory {
         }
 
         var options = {
-            'method': 'GET',
-            'url': `${this.apiBaseUrl}/monitor-updates/poll`,
+            'method': 'POST',
+            'url': `${this.apiBaseUrl}/monitor-updates`,
             'headers': {
                 'Accept': 'application/json',
+                'Content-Type': 'application/json',
                 'Authorization': `Bearer ${this.apiToken}`
-            }
+            },
+            'body': JSON.stringify([
+                {
+                    "ID": "AreaStateRequest",
+                    "RequestType": "MonitorEntityStates",
+                    "InputData": {
+                        "stateType": "AreaState",
+                        "timeSinceUpdate": "0"
+                    }
+                },
+                {
+                    "ID": "InputStateRequest",
+                    "RequestType": "MonitorEntityStates",
+                    "InputData": {
+                        "stateType": "InputState",
+                        "timeSinceUpdate": "0"
+                    }
+                },
+                {
+                    "ID": "OutputStateRequest",
+                    "RequestType": "MonitorEntityStates",
+                    "InputData": {
+                        "stateType": "OutputState",
+                        "timeSinceUpdate": "0"
+                    }
+                }
+            ])
         };
 
         request(options, (error, response, body) => {
             if (error) {
                 this.log('[ERROR] Failed to poll state:', error);
+            } else if (response.statusCode !== 200) {
+                this.log(`[ERROR] Polling request failed with status ${response.statusCode}:`, body);
             } else if (!body || body.trim() === "") {
                 this.log('[WARNING] Poll request returned an empty response. Retrying...');
             } else {
@@ -102,7 +131,7 @@ class InceptionAccessory {
                     if (parsedBody.Updates) {
                         parsedBody.Updates.forEach(update => {
                             if (update.Type === 'AreaStateUpdate' && update.AreaId === this.areaId) {
-                                this._updateHomeKitState(update.State);
+                                this.updateHomeKitState(update.State);
                             }
                         });
                     }
@@ -112,29 +141,5 @@ class InceptionAccessory {
             }
             setTimeout(() => this.pollState(), 5000);
         });
-    }
-
-    _updateHomeKitState(stateValue) {
-        const isArmed = stateValue & 1;
-        const isAlarm = stateValue & 2; // 0x00000002 - Area is in alarm
-        const isStayArm = stateValue & 512;
-        const isNightArm = stateValue & 1024;
-        const isDisarmed = stateValue & 2048;
-
-        let homekitState = Characteristic.SecuritySystemCurrentState.DISARMED;
-        if (isAlarm) {
-            homekitState = Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED;
-        } else if (isNightArm) {
-            homekitState = Characteristic.SecuritySystemCurrentState.NIGHT_ARM;
-        } else if (isStayArm) {
-            homekitState = Characteristic.SecuritySystemCurrentState.STAY_ARM;
-        } else if (isArmed) {
-            homekitState = Characteristic.SecuritySystemCurrentState.AWAY_ARM;
-        }
-
-        this.log(`[INFO] Updating HomeKit state to: ${homekitState}`);
-        this.service
-            .getCharacteristic(Characteristic.SecuritySystemCurrentState)
-            .updateValue(homekitState);
     }
 }
